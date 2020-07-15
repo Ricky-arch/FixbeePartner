2ab7dcee839a4590da991831acaa17eeb5e40d97
Ricky-arch <srvsutradhar7@gmail.com>
2020-07-15 18:31:40 +0530

    Display picture fixed

--- lib/events/display_picture_event.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fixbee_partner/events/event.dart';
-
-class DisplayPictureEvent extends Event{
-  DisplayPictureEvent(int eventId) : super(eventId);
-  static final DisplayPictureEvent uploadDisplayPicture= DisplayPictureEvent(100);
-  static final DisplayPictureEvent downloadDisplayPicture=DisplayPictureEvent(101);
-}
--- lib/models/display_picture_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:fixbee_partner/models/view_model.dart';
-
-class DisplayPictureModel extends ViewModel{
-  String imageUrl;
-}
